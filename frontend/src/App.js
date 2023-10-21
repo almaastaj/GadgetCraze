81857055577bcbc170cc53f09c15ad22ad37f250
Almaas Taj <almaastaj123@gmail.com>
2023-10-21 23:56:44 +0530

    Complete initial frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 const App = () => {
     return (
         <>
             <Header />
             <main className="py-3">
                 <Container>
-                    <h1>Welcome to Gadget Craze</h1>
+                    <Outlet />
                 </Container>
             </main>
             <Footer />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
+
 const Header = () => {
     return (
         <header id="navbar">
             <Navbar style={{ backgroundColor: "#713ABE" }} variant="dark" expand="md" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">
-                        <img src={logo} alt="logo of Gadget Craze" />
-                        GadgetCraze
-                    </Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>
+                            <img src={logo} alt="logo of Gadget Craze" />
+                            GadgetCraze
+                        </Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="/cart">
-                                <FaShoppingCart /> Cart
-                            </Nav.Link>
-                            <Nav.Link href="/login">
-                                <FaUser /> Sign In
-                            </Nav.Link>
+                            <LinkContainer to="/cart">
+                                <Nav.Link>
+                                    <FaShoppingCart /> Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link>
+                                    <FaUser /> Sign In
+                                </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
