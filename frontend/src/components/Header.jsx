df32faea8ce726fedc14d68f7a595e740ef71ca4
Almaas Taj <almaastaj123@gmail.com>
2023-10-21 17:01:27 +0530

    Complete E-Commerce site till Products

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
+const Header = () => {
+    return (
+        <header id="navbar">
+            <Navbar style={{ backgroundColor: "#713ABE" }} variant="dark" expand="md" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img src={logo} alt="logo of Gadget Craze" />
+                        GadgetCraze
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="/cart">
+                                <FaShoppingCart /> Cart
+                            </Nav.Link>
+                            <Nav.Link href="/login">
+                                <FaUser /> Sign In
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+};
+
+export default Header;
