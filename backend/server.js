c52b04dd76680d7c172f21fc1e4c30d07adcbbfc
Almaas Taj <almaastaj123@gmail.com>
2023-10-22 13:29:19 +0530

    Complete till Data seeding in DB

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 const port = process.env.PORT;
 
+connectDB(); // Connect to MongoDB
 const app = express();
 
 app.get("/", (req, res) => {
