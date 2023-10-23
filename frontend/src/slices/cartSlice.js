50f21ba937ef79ebc08ec8cbb695bd97c827732a
Almaas Taj <almaastaj123@gmail.com>
2023-10-23 15:52:18 +0530

    Complete Cart Screen and remove Cart func

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -19,11 +19,18 @@ const cartSlice = createSlice({
                 // If not exists, add new item to cartItems
                 state.cartItems = [...state.cartItems, item];
             }
-            // Update the cart state using the updateCart function
+            // Update the prices and save to storage
+            return updateCart(state);
+        },
+        removeFromCart: (state, action) => {
+            // Filter out the item to remove from the cart
+            state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+
+            // Update the prices and save to storage
             return updateCart(state);
         },
     },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
