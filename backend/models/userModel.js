df1239971139cdba1c899bd180947fcaa602b94e
Almaas Taj <almaastaj123@gmail.com>
2023-10-23 17:27:54 +0530

    Complete email & password validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
     {
@@ -11,6 +12,10 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     },
 );
+
+userSchema.methods.matchPassword = async function (enterPassword) {
+    return await bcrypt.compare(enterPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,22 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT;
 
 connectDB(); // Connect to MongoDB
 const app = express();
+// Body Parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
