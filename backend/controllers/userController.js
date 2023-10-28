b8169b47e0eaee632d00cf2dd1af9b8337b07b8b
Almaas Taj <almaastaj123@gmail.com>
2023-10-28 21:47:15 +0530

    Complete Reviews Functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -102,7 +102,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 
         const updatedUser = await user.save();
 
-        res.staus(200).json({
+        res.status(200).json({
             _id: updatedUser._id,
             name: updatedUser.name,
             email: updatedUser.email,
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,11 +5,13 @@ import {
     createProduct,
     updateProduct,
     deleteProduct,
+    createProductReview,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id").get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
